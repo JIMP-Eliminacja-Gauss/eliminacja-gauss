e547df4b7e4afbdfdef047b127793bba5e675de9
Adam Smerdzyński <adamsm0002@gmail.com>
2021-12-09 19:01:31 +0100

    Testowy gauss

---
+++ gaussTEST.c
@@ -0,0 +1,44 @@
+#include "gauss.h"
+
+/**
+ * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ */
+int eliminate(Matrix *mat, Matrix *b){
+    int n;
+    int k;
+    int r;
+    int c;
+
+/*     1 1 1 1 1
+*      0 1 1 1 1    
+*      0 0 1 1 1
+*      0 0 0 1 1
+*      0 0 0 0 1
+*      0 0 0 0 0        
+*
+*      [r] - wiersze    [c] - kolumny    
+*       r, c,   **data   
+*      A[i][j]   A[i+1][j]      A[i+1] - (A[i] *  A[i+1] / A[i])  (A[i][j] * (A[i+1][j] / A[i][j]))   */
+
+    for( r = 1; r < (mat->r); r++ ) {
+        n = 1;
+        k = r;
+        if( r > mat->c )
+            k = mat->c;
+        /*zerowanie pierwszego elementu w wierszu*/
+        for( c = 0; c < (mat->c); c++ ) {
+            mat->data[r][c] = mat->data[r][c] - ( mat->data[r-1][c] * ( mat->data[r][0] / mat->data[r-1][0] );
+        }
+        b[r][0] = b[r][0] - ( b[r-1][0] * ( mat->data[r][0] / mat->data[r-1][0] ) );
+        while( n != k ) {  /* bo n maksymalnie moze byc rowne (c-1) zeby nie mazalo po pamieci */
+            for( c = 0; c < (mat->c); c++ )  {
+            mat->data[r][c] = mat->data[r][c] - ( mat->data[n][c] * ( mat->data[r][n] / mat->data[n][n] ) )
+            }
+            b[r][0] = b[r][0] - ( b[n][0] * ( mat->data[r][n] / mat->data[n][n] ) );
+            n++;
+        }
+    }
+		return 0;
+}
+
