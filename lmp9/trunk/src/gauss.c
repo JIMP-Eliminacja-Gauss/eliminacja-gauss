0a33b89d1df3239d72f8db55abaa65a80aaf5efc
Adam Smerdzyński <adamsm0002@gmail.com>
2021-12-14 20:51:16 +0100

    Dodanie wyboru elementu diagonalnego

--- lmp9/trunk/src/gauss.c
+++ lmp9/trunk/src/gauss.c
@@ -1,23 +1,55 @@
 #include "gauss.h"
-
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+int diag_elem_choice( Matrix *mat, Matrix *b, int j ) {
+    int max = mat->data[j][j];
+    int row = j;
+    for( int i = j+1; i < mat->r; i++ ) {
+        if( fabs(max) < fabs(mat->data[i][j]) ) {
+            row = i;
+            max = mat->data[i][j];
+        }
+    }
+    if( mat->data[row][j] == 0 )
+        return -1;
+    if( row == j )
+        return -2;
+    return row;
+}
+
+
 int eliminate(Matrix *mat, Matrix *b){
     int i;
     int j;
     int n;
-
+    double temp;
+    
     for( i = 0; i < (mat->r - 1); i++ ) {
-        if( mat->r == mat->c && mat->data[i][i] == 0)
+        n = diag_elem_choice( mat, b, i );
+        if( n == -1 )
             return 1;
+        if( n == -2 ) {
+            ;
+        } else {
+            temp = b->data[i][0];
+            b->data[i][0] = b->data[n][0];
+            b->data[n][0] = temp;
+            for( j = 0; j < mat->c; j++ ) {
+                temp = mat->data[i][j];
+                mat->data[i][j] = mat->data[n][j];
+                mat->data[n][j] = temp;
+            }
+        }
+        
         for( j = i+1; j < mat->r;  j++ ) {
-            double temp = mat->data[j][i] / mat->data[i][i];
+            temp = mat->data[j][i] / mat->data[i][i];
             b->data[j][0] = b->data[j][0] - temp*(b->data[i][0]);
             for( n = 0; n < mat->c; n++ )
-                mat->data[j][n] = mat->data[j][n] - temp*(mat->data[i][n]);
-            
+                mat->data[j][n] = mat->data[j][n] - temp*(mat->data[i][n]);    
         }
     }
     return 0;
